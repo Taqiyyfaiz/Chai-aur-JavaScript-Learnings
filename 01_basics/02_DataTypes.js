14ec4e5863ba3d94ce4f963835c831fa7d44a7a3
Taqiyy Faiz <taqiyyfaizm@gmail.com>
2024-03-08 19:18:43 +0530

    DataTypes

---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) we are using node Js, not browser
+console.log(245 % 6);
+
+console.log("Now am Learning JavaScript"); // To Print
+
+
+let name = "Taqiyy" //string Datatype
+let age = "18"
+let isloggedIn = false //boolean Datatype
+
+// bigint (if the number is too big. a big market like facebook, stocks)
+// boolean => true/false
+// null means empty. is a type (stand alone value)
+// undefined => 
+// symbol => unique
+
+
+//object
+
+console.log(typeof undefined); //undefined because it is a type
+console.log(typeof null); //object
