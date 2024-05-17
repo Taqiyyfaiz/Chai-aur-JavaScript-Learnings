095c693c6abe6f1538ffb289870c5858c4fa1447
Taqiyy Faiz <taqiyyfaizm@gmail.com>
2024-05-17 19:55:51 +0530

    .

---
+++ 01_basics/06_Nums_and_Maths.js
@@ -0,0 +1,52 @@
+const Score = 197
+// console.log(Score);
+
+const ScoreToWin = new Number(18)
+// console.log(ScoreToWin);
+
+// 
+// console.log(ScoreToWin.toString().length);
+
+// you will use amny times speciallt in e-commerce application
+// console.log(ScoreToWin.toFixed(2));
+
+const OtherScore = 25.8950
+// toPrecison is used to find the approximate value
+// console.log(OtherScore.toPrecision(3));
+
+const Ticket = 70000000
+
+// toLocalString is used make 
+// console.log(Ticket.toLocaleString('en-IN'));
+
+// ***************************Maths**********************************//
+
+//
+console.log(Math);
+
+// .abs value is used to make negative value into positiove value
+console.log(Math.abs(-4));
+
+// .round 
+console.log(Math.round(4.6));
+
+// it will choose the top numbers
+console.log(Math.ceil(4.6));
+
+// floor is used to get the down or lowest value
+console.log(Math.floor(4.9));
+
+// it will give you the value between 0 to 1 
+console.log((Math.random()*10) + 1);
+
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+// formula 
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+console.log();
+
+
+
