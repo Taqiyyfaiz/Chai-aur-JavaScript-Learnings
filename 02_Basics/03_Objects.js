595477cf55f5832e04991dc36a1b02337d50f0d8
Taqiyy Faiz <113523139+Taqiyyfaiz@users.noreply.github.com>
2024-08-12 14:18:02 +0000

    json Talk

--- 02_Basics/03_Objects.js
+++ 02_Basics/03_Objects.js
@@ -39,3 +39,4 @@ JsUser.greeting2 = function() {
 
 console.log(JsUser.greeting());
 console.log(JsUser.greeting2()); 
+
---
+++ 02_Basics/04_Objects.js
@@ -0,0 +1,15 @@
+const course = {
+    coursename : "js in hindi",
+    price:"999",
+    courseInstructer:"MTF"
+}
+
+// Course.courseIntructer
+
+const {courseInstructer} = course
+console.log(courseInstructer);
+
+// What is JSON
+// {
+//     name:"MTF",        
+// }
