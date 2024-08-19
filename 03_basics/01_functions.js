e92bff4b7a7158f9aff99815007a1b17912e8999
Taqiyyfaiz <taqiyyfaizm@gmail.com>
2024-08-19 18:01:00 +0530

    Functions with Objects

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -27,5 +27,35 @@ function loginUserMesg(username) {
 }
 
 // console.log(loginUserMesg("Taqi Faiz"))
-console.log(loginUserMesg());
+// console.log(loginUserMesg());
+
+// Functions with objects
+
+// ... Rest or Spread Operator it will be based on uses
+function calculateCardPrice(...num1) {
+  return num1
+}
+// ... It will print the multiple values into array
+// console.log(calculateCardPrice(200,300, 400));
+
+const user = {
+  username: "Taqi",
+  price: "999"
+}
+
+function handleObject(anyObject) {
+  console.log(`username is ${anyObject.username} and price is ${anyObject.price}`);
+  
+}
+
+handleObject(user)
+
+
+const myNewArray = [2, 5, 7, 10]
+
+function returnThirdValue(myArray) {
+  return myArray[3]
+}
+console.log(returnThirdValue(myNewArray));
+
 
